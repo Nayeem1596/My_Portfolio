53a890128bbc0fdf7097720c495604a46cbaec84
Nayeem1596 <nayeemkhanla@gmail.com>
2026-02-03 20:31:01 +0530

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,38 @@
+import dayjs from "dayjs";
+
+import { navLinks, navIcons } from "@constants/index.js";
+import React from "react";
+
+const Navbar = () => {
+	return (
+		<nav>
+			<div>
+				<img src="/images/logo.svg" alt="logo" />
+				<p className="font-bold">Nayeem Space</p>
+				<ul>
+					{navLinks.map((item) => (
+						<li key={item.id}>{item.name}</li>
+					))}
+				</ul>
+			</div>
+
+			<div>
+				<ul>
+					{navIcons.map(({ id, img }) => (
+						<li key={id}>
+							<img
+								src={img}
+								className="icon-hover"
+								alt={`icon-${id}`}
+							/>
+						</li>
+					))}
+				</ul>
+
+				<time>{dayjs().format("ddd D MMM h:mm A")}</time>
+			</div>
+		</nav>
+	);
+};
+
+export default Navbar;
