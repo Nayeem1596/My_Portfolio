38a1d92b8cdf339fe910d99cc5ce93542840e132
Nayeem1596 <nayeemkhanla@gmail.com>
2026-02-04 23:57:15 +0530

    implement safari window

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import gsap from "gsap";
 
 import React from "react";
 import { Navbar, Welcome, Dock } from "@components";
-import { Terminal } from "@windows";
+import { Terminal, Safari } from "@windows";
 
 gsap.registerPlugin(Draggable);
 
@@ -15,6 +15,7 @@ const App = () => {
 			<Dock />
 
 			<Terminal />
+			<Safari />
 		</main>
 	);
 };
