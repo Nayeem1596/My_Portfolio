46e76d087cecda9d82ed1d17558c1c9fabb7358f
Nayeem1596 <nayeemkhanla@gmail.com>
2026-02-04 21:03:37 +0530

    Revert "implement store"
    This reverts commit ef166ce168895e54aa3e992fda3e3a1113683a5d.

--- src/components/Dock.jsx
+++ src/components/Dock.jsx
@@ -4,10 +4,8 @@ import gsap from "gsap";
 
 import { dockApps } from "@constants/index.js";
 import { useGSAP } from "@gsap/react";
-import useWindowStore from "@store/window.js";
 
 const Dock = () => {
-	const { openWindow, closeWindow, windows } = useWindowStore();
 	const dockRef = useRef(null);
 
 	useGSAP(() => {
@@ -57,21 +55,8 @@ const Dock = () => {
 		};
 	}, []);
 
-	const toggleApp = (app) => {
-		if (!app.canOpen) return;
-
-		const window = windows[app.id];
-
-		if (!window) {
-			console.error(`Window not found for app: ${app.id}`);
-			return;
-		}
-
-		if (window.isOpen) {
-			closeWindow(app.id);
-		} else {
-			openWindow(app.id);
-		}
+	const toggleApp = () => {
+		// TODO: Implement app toggle logic
 	};
 
 	return (
